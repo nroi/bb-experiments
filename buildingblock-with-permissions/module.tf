7f8d179bed3ca0d1fb87b644b35f8a4d428ea7eb
Fabian Muscariello <fmuscariello@meshcloud.io>
2026-02-10 16:53:18 +0100

    feat: add bb with permissions

---
+++ buildingblock-with-permissions/module.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    meshstack = {
+      source  = "meshcloud/meshstack"
+      version = ">= 0.17.0"
+    }
+  }
+}
+
+provider "meshstack" {
+}
+
+variable "project_identifier" {
+  type        = string
+  description = "The identifier of the meshStack project to be created."
+}
+
+variable "workspace_identifier" {
+  type        = string
+  description = "The identifier of the meshStack workspace."
+}
+
+variable "paymentmethod_identifier" {
+  type        = string
+  description = "The identifier of the meshStack payment method."
+}
+
+resource "meshstack_project" "example" {
+  metadata = {
+    name               = var.project_identifier
+    owned_by_workspace = var.workspace_identifier
+  }
+  spec = {
+    payment_method_identifier = var.paymentmethod_identifier
+    display_name              = "My Project ${var.project_identifier}"
+    # TODO perhaps this shouldn't be hardcoded? but we also can't just leave it empty, because tags are required in our dev environment.
+    tags = {
+      environment     = ["qa"]
+      confidentiality = ["Internal"]
+    }
+  }
+}
+
+resource "meshstack_tenant" "sr_global" {
+  metadata = {
+    owned_by_workspace  = meshstack_project.example.metadata.owned_by_workspace
+    owned_by_project    = meshstack_project.example.metadata.name
+    platform_identifier = "sr.global"
+  }
+
+  spec = {
+    # landing_zone_identifier is optional for SERVICEREGISTRY platform type
+  }
+}
